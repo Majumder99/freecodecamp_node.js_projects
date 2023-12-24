a40a86a97b328566f9894599619fc5621ac0b0a4
Sourav Majumder <souravmajumderk99@gmail.com>
2023-12-25 03:09:31 +0600

    please accept now

--- Timestamp Microservice/index.js
+++ Timestamp Microservice/index.js
@@ -1,54 +1,54 @@
-const express = require("express");
-const app = express();
-require("dotenv").config();
-const cors = require("cors");
+// index.js
+// where your node app starts
+
+// init project
+var express = require("express");
+var app = express();
+
+// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
+// so that your API is remotely testable by FCC
+var cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
+// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-// Routing
+// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(`${__dirname}/views/index.html`);
+  res.sendFile(__dirname + "/views/index.html");
 });
 
-// app.get("/api/", function (req, res) {
-//   res.json({ greeting: "Oh, you've found this! Well, congrats! :p" });
-// });
-
-app.get("/api/", function (req, res) {
-  const date = new Date();
-
-  res.json({
-    unix: date.valueOf(),
-    utc: date.toUTCString(),
-  });
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.get("/api/:dateParam", function (req, res) {
-  let dateParam = req.params.dateParam;
-
-  if (/^\d{5,}$/.test(dateParam)) dateParam = parseInt(dateParam);
-
-  const date = new Date(dateParam);
-
-  if (date.toString() == "Invalid Date") {
-    res.json({
-      error: "Invalid Date",
-    });
+app.get("/api", function (req, res) {
+  let currentUnix = new Date().getTime();
+  let currentUtc = new Date().toUTCString();
+  res.json({ unix: currentUnix, utc: currentUtc });
+});
+/*
+Extra Comments
+*/
+app.get("/api/:date_string", (req, res) => {
+  let dateString = req.params.date_string;
+  let unixPattern = /\d{5,}/;
+  if (unixPattern.test(dateString)) {
+    let dateNum = parseInt(dateString);
+    res.json({ unix: dateNum, utc: new Date(dateNum).toUTCString() });
   } else {
-    res.json({
-      unix: date.valueOf(),
-      utc: date.toUTCString(),
-    });
+    let inputDate = new Date(dateString);
+    if (inputDate.toString() === "Invalid Date") {
+      res.json({ error: "Invalid Date" });
+    } else {
+      res.json({ unix: inputDate.getTime(), utc: inputDate.toUTCString() });
+    }
   }
 });
+/* comments */
 
-// No matching route
-app.use(function (req, res, next) {
-  res.status(404).sendFile(`${__dirname}/views/404.html`);
-});
-
-// Listening for requests
-const listener = app.listen(process.env.PORT, function () {
+// listen for requests :)
+var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
