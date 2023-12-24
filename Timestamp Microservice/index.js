27cccd07cc9bc5c2f94e2b70370948ff098ce8be
Sourav Majumder <souravmajumderk99@gmail.com>
2023-12-25 03:02:36 +0600

    nothing

--- Timestamp Microservice/index.js
+++ Timestamp Microservice/index.js
@@ -1,56 +1,54 @@
-// index.js
-// where your node app starts
+const express = require("express");
+const app = express();
 
-// init project
-var express = require("express");
-var app = express();
-
-// enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-require("dotenv").config();
+const cors = require("cors");
 app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
 
-app.get("/api/:date?", (req, res) => {
-  let dateString = req.params.date;
-
-  var date = new Date();
-  // if the given parameter is a number (timestamp)
-  if (/^\d*$/.test(dateString)) {
-    date.setTime(dateString);
-  }
-  // else we just create a new date parsing the string given
-  else {
-    date = new Date(dateString);
-  }
-
-  // Check if the timestamp is valid
-  res.set({ "Content-Type": "application/json" });
-  if (!date.getTime())
-    res.send(JSON.stringify({ error: "Invalid date given" }));
-  // else, we send the object with two members (unix and natural)
-  else
-    res.send(
-      JSON.stringify({
-        unix: date.getTime(),
-        natural: strftime("%B %d, %Y", date),
-      })
-    );
-});
-
-// http://expressjs.com/en/starter/basic-routing.html
+// Routing
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(`${__dirname}/views/index.html`);
 });
 
-// your first API endpoint...
-app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+app.get("/api/easteregg", function (req, res) {
+  res.json({ greeting: "Oh, you've found this! Well, congrats! :p" });
 });
-// listen for requests :)
-var listener = app.listen(process.env.PORT, function () {
+
+app.get("/api/timestamp", function (req, res) {
+  const date = new Date();
+
+  res.json({
+    unix: date.valueOf(),
+    utc: date.toUTCString(),
+  });
+});
+
+app.get("/api/timestamp/:dateParam", function (req, res) {
+  let dateParam = req.params.dateParam;
+
+  if (/^\d{5,}$/.test(dateParam)) dateParam = parseInt(dateParam);
+
+  const date = new Date(dateParam);
+
+  if (date.toString() == "Invalid Date") {
+    res.json({
+      error: "Invalid Date",
+    });
+  } else {
+    res.json({
+      unix: date.valueOf(),
+      utc: date.toUTCString(),
+    });
+  }
+});
+
+// No matching route
+app.use(function (req, res, next) {
+  res.status(404).sendFile(`${__dirname}/views/404.html`);
+});
+
+// Listening for requests
+const listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
 });
