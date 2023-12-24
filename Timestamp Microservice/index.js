0b8f210600bb78eaa5c9bf49bc2168047dfd560d
Sourav Majumder <souravmajumderk99@gmail.com>
2023-12-25 03:20:46 +0600

    l

--- Timestamp Microservice/index.js
+++ Timestamp Microservice/index.js
@@ -1,14 +1,14 @@
-// index.js
+// server.js
 // where your node app starts
 
 // init project
 var express = require("express");
 var app = express();
-require("dotenv").config();
+
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC
 var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+app.use(cors({ optionSuccessStatus: 200 })); // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static("public"));
@@ -18,24 +18,43 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + "/views/index.html");
 });
 
-app.get("/api/:date_string", (req, res) => {
-  let dateString = req.params.date_string;
-  let unixPattern = /\d{5,}/;
-  if (unixPattern.test(dateString)) {
-    let dateNum = parseInt(dateString);
-    res.json({ unix: dateNum, utc: new Date(dateNum).toUTCString() });
-  } else {
-    let inputDate = new Date(dateString);
-    if (inputDate.toString() === "Invalid Date") {
-      res.json({ error: "Invalid Date" });
-    } else {
-      res.json({ unix: new Date().getTime(), utc: new Date().toUTCString() });
-    }
-  }
+// your first API endpoint...
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
-/* comments */
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log("Your app is listening on port " + listener.address().port);
+});
+
+let responseObject = {};
+
+app.get("/api/timestamp/:input", (request, response) => {
+  let input = request.params.input;
+
+  if (input.includes("-")) {
+    /* Date String */
+    responseObject["unix"] = new Date(input).getTime();
+    responseObject["utc"] = new Date(input).toUTCString();
+  } else {
+    /* Timestamp */
+    input = parseInt(input);
+
+    responseObject["unix"] = new Date(input).getTime();
+    responseObject["utc"] = new Date(input).toUTCString();
+  }
+
+  if (!responseObject["unix"] || !responseObject["utc"]) {
+    response.json({ error: "Invalid Date" });
+  }
+
+  response.json(responseObject);
+});
+
+app.get("/api/timestamp", (request, response) => {
+  responseObject["unix"] = new Date().getTime();
+  responseObject["utc"] = new Date().toUTCString();
+
+  response.json(responseObject);
 });
