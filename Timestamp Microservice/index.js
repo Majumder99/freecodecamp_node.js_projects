f78c04b961fc10ec9ca26dbf51ea5c2b95fa1252
Sourav Majumder <souravmajumderk99@gmail.com>
2023-12-25 15:05:40 +0600

    m

--- Timestamp Microservice/index.js
+++ Timestamp Microservice/index.js
@@ -30,7 +30,7 @@ var listener = app.listen(process.env.PORT, function () {
 
 let responseObject = {};
 
-app.get("/api/timestamp/:input", (request, response) => {
+app.get("/api/:input", (request, response) => {
   let input = request.params.input;
 
   if (input.includes("-")) {
@@ -52,7 +52,7 @@ app.get("/api/timestamp/:input", (request, response) => {
   response.json(responseObject);
 });
 
-app.get("/api/timestamp", (request, response) => {
+app.get("/api/", (request, response) => {
   responseObject["unix"] = new Date().getTime();
   responseObject["utc"] = new Date().toUTCString();
 
