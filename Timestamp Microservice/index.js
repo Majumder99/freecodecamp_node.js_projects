62f5f13021026a1337bb5584f8918010ea4f43c1
Sourav Majumder <souravmajumderk99@gmail.com>
2023-12-29 23:04:09 +0600

    error fix

--- Timestamp Microservice/index.js
+++ Timestamp Microservice/index.js
@@ -5,15 +5,23 @@ app.use(express.static("public"));
 
 app.get("/api/:date?", (req, res) => {
   let date;
+
+  // Check if a date parameter was provided
   if (req.params.date) {
+    // Check if the date parameter is a valid number (Unix timestamp)
     const unixTimestamp = parseInt(req.params.date);
-    date = isNaN(unixTimestamp)
-      ? new Date(req.params.date)
-      : new Date(unixTimestamp);
+    if (!isNaN(unixTimestamp)) {
+      date = new Date(unixTimestamp);
+    } else {
+      // Handle non-numeric date strings
+      date = new Date(req.params.date);
+    }
   } else {
+    // No date parameter, use current date
     date = new Date();
   }
 
+  // Check if the date is valid
   if (isNaN(date.getTime())) {
     res.json({ error: "Invalid Date" });
   } else {
